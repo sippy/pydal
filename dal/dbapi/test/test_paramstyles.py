317e71d64c90928b1625f86e764b52a87cf46aa4
pbuschman <pbuschman@users.noreply.sourceforge.net>
2004-10-01 15:37:24 +0000

    Added basic segmentize and paramstyle tests to test_paramstyles.py...getting the hang of unittest now.

--- dal/dbapi/test/test_paramstyles.py
+++ dal/dbapi/test/test_paramstyles.py
@@ -2,20 +2,65 @@
 Peter's first stab at a unit test
 """
 import unittest
+from dal.dbapi.paramstyles import *
 
-class FakeTests(unittest.TestCase):
+SEGMENTIZE_TESTS = [
+    [ r'S1', ['S1'] ],
+    [ r'"S1"S2', ['"S1"', 'S2'] ],
+]
+
+PARAMSTYLE_TESTS = [
+    {
+        'from'            : 'qmark',
+        'to'              : 'qmark',
+        'input_query'     : '? ?',
+        'expected_query'  : '? ?',
+        'input_params'    : [ 1, 2 ],
+        'expected_params' : [ 1, 2 ],
+    },
+    {
+        'from'            : 'qmark',
+        'to'              : 'format',
+        'input_query'     : '? ?',
+        'expected_query'  : '%s %s',
+        'input_params'    : [ 1, 2 ],
+        'expected_params' : [ 1, 2 ],
+    },
+]
+
+class SegmentizeTests(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.tests = SEGMENTIZE_TESTS
 
     def tearDown(self):
-        pass
+        del self.tests
 
-    def testA(self):
-        assert 2 + 2 == 4, 'It isn''t 1984 yet!'
+    def testSegments(self):
+        for test in self.tests:
+            input    = test[0]
+            expected = test[1]
+            actual   = segmentize(input)
+            assert actual == expected, 'input=%s, expected=%s, actual=%s' % (input, expected, actual)
 
-    def testB(self):
-        assert 2 + 2 == 5, 'Big Brother is watching you!'
+class ParamstyleTests(unittest.TestCase):
+
+    def setUp(self):
+        self.tests = PARAMSTYLE_TESTS
+
+    def tearDown(self):
+        del self.tests
+
+    def testParamstyles(self):
+        for test in self.tests:
+            from_paramstyle = test['from']
+            to_paramstyle   = test['to']
+            input_query     = test['input_query']
+            expected_query  = test['expected_query']
+            input_params    = test['input_params']
+            expected_params = test['expected_params']
+            actual_query, actual_params = convert( from_paramstyle, to_paramstyle, input_query, input_params )
+            assert actual_query == expected_query, 'from=%s, to=%s, input_query=%s, input_params=%s, expected_query=%s, expected_params=%s, actual_query=%s, actual_params=%s' % (from_paramstyle, to_paramstyle, input_query, input_params, expected_query, expected_params, actual_query, actual_params)
 
 
 if __name__ == '__main__':
