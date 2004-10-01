9551f1d4ffe657dd29c71e3637d90eb60f974b9d
pbuschman <pbuschman@users.noreply.sourceforge.net>
2004-10-01 17:03:38 +0000

    More paramstyle tests...

--- dal/dbapi/test/test_paramstyles.py
+++ dal/dbapi/test/test_paramstyles.py
@@ -20,11 +20,35 @@ PARAMSTYLE_TESTS = [
     },
     {
         'from'            : 'qmark',
+        'to'              : 'numeric',
+        'input_query'     : '? ?',
+        'expected_query'  : ':1 :2',
+        'input_params'    : [ 1, 2 ],
+        'expected_params' : [ 1, 2 ],
+    },
+    {
+        'from'            : 'qmark',
+        'to'              : 'named',
+        'input_query'     : '? ?',
+        'expected_query'  : ':param1 :param2',
+        'input_params'    : [ 1, 2 ],
+        'expected_params' : { 'param1' : 1, 'param2' : 2 },
+    },
+    {
+        'from'            : 'qmark',
         'to'              : 'format',
         'input_query'     : '? ?',
         'expected_query'  : '%s %s',
         'input_params'    : [ 1, 2 ],
         'expected_params' : [ 1, 2 ],
+    },
+    {
+        'from'            : 'qmark',
+        'to'              : 'pyformat',
+        'input_query'     : '? ?',
+        'expected_query'  : '%(param1)s %(param2)s',
+        'input_params'    : [ 1, 2 ],
+        'expected_params' : { 'param1' : 1, 'param2' : 2 },
     },
 ]
 
@@ -60,7 +84,7 @@ class ParamstyleTests(unittest.TestCase):
             input_params    = test['input_params']
             expected_params = test['expected_params']
             actual_query, actual_params = convert( from_paramstyle, to_paramstyle, input_query, input_params )
-            assert actual_query == expected_query, 'from=%s, to=%s, input_query=%s, input_params=%s, expected_query=%s, expected_params=%s, actual_query=%s, actual_params=%s' % (from_paramstyle, to_paramstyle, input_query, input_params, expected_query, expected_params, actual_query, actual_params)
+            assert actual_query == expected_query and actual_params == expected_params, 'from=%s, to=%s, input_query=%s, input_params=%s, expected_query=%s, expected_params=%s, actual_query=%s, actual_params=%s' % (from_paramstyle, to_paramstyle, input_query, input_params, expected_query, expected_params, actual_query, actual_params)
 
 
 if __name__ == '__main__':
